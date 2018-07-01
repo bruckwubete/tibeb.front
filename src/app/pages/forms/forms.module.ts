93f69c0f4d75da5f23d4478a72cff2bae95a452d
bruckwubete <bruckwendwessenwubet@cmail.carleton.ca>
2018-06-30 21:22:32 -0400

    adding create actor form and expandable tabs

--- src/app/pages/forms/forms.module.ts
+++ src/app/pages/forms/forms.module.ts
@@ -3,12 +3,14 @@ import { NgModule } from '@angular/core';
 import { ThemeModule } from '../../@theme/theme.module';
 import { FormsRoutingModule, routedComponents } from './forms-routing.module';
 import { MyDatePickerModule } from 'mydatepicker';
+import { CollapsibleModule } from 'angular2-collapsible';
 
 @NgModule({
   imports: [
     ThemeModule,
     FormsRoutingModule,
-    MyDatePickerModule
+    MyDatePickerModule,
+    CollapsibleModule
   ],
   declarations: [
     ...routedComponents,
