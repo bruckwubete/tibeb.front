d56db329b8906d95bf1c89d0e4b937d99130ec16
bruckwubete <bruckwendwessenwubet@cmail.carleton.ca>
2018-10-22 01:16:12 -0400

    added calendar to movie view

--- src/app/pages/movies/movies.module.ts
+++ src/app/pages/movies/movies.module.ts
@@ -5,12 +5,14 @@ import { ThemeModule } from '../../@theme/theme.module';
 import { MovieRoutingModule, routedComponents } from './movies-routing.module';
 import { Backdrop } from './backdrop/backdrop.component'
 import { KitRatingComponent } from './rating/kit-rating.component'
+import { MomentModule } from 'angular2-moment';
 
 @NgModule({
   imports: [
     ThemeModule,
     MovieRoutingModule,
     Ng2SmartTableModule,
+    MomentModule
   ],
   declarations: [
     ...routedComponents,
